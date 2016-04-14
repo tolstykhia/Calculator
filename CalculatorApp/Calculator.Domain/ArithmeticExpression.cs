adbf12968e199e9ecbb4a412a2f936d35f8f541b
tolstykhia <tolstykhia89@gmail.com>
2016-04-14 12:13:10 +0600

    добавлен проект Calculator.Domain

---
+++ CalculatorApp/Calculator.Domain/ArithmeticExpression.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.Domain;
+
+namespace Calculator.Domain
+{
+    public class ArithmeticExpression
+    {
+        public Decimal x { get; set; }
+        public Decimal y { get; set; }
+
+        public OperationType Operator { get; set; }
+
+    }
+}
