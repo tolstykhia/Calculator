519fc7e2827373874db0fdf131df34fa3d98ff8d
tolstykhia <tolstykhia89@gmail.com>
2016-04-14 09:28:54 +0600

    добавлен проект Calculator.UI

---
+++ CalculatorApp/Calculator.UI/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Optimization;
+
+namespace Calculator.UI
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            
+        }
+    }
+}
