5a839beffba723ddaea1b6eba51381f96ebb064c
tolstykhia <tolstykhia89@gmail.com>
2016-04-19 12:13:05 +0600

    добавлена функциональнось для рабоы с калькулятором через Web-интерфейс

---
+++ CalculatorApp/Calculator.UI/Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculator.Services;
+using Ninject;
+
+namespace Calculator.UI.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private readonly ICalculatorExecuter _calculator;
+
+        public CalculatorController(ICalculatorExecuter calculator)
+        {
+            this._calculator = calculator;
+        }
+
+        public ActionResult _CalculatorView()
+        {
+            return PartialView("_CalculatorView");
+        }
+
+        public ActionResult GetDecision(String expression)
+        {
+            var result = _calculator.GetDecision(expression);
+
+            if (result == null) return Json("Error", JsonRequestBehavior.AllowGet);
+
+            return Json(result.Value, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
